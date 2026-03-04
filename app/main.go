4d5281aaab81271c4104c09c34a50bfabfc7b32c
Firmeteran <mfirmansyah12345@gmail.com>
2026-03-04 13:07:46 +0700

    main: User function integration

--- app/main.go
+++ app/main.go
@@ -3,15 +3,32 @@ package main
 import (
 	"os"
 	"sport-rental/config"
+	"sport-rental/controller"
+	"sport-rental/models"
+	"sport-rental/repository"
+	"sport-rental/service"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
 	// Connect to database
 	config.InitDB()
+	config.DB.AutoMigrate(&models.User{}, &models.Equipment{}, &models.RentalHistory{})
 
 	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Dependency Injection
+	userRepo := repository.NewUserRepo(config.DB)
+	userSvc := service.NewUserService(userRepo)
+	userCtrl := controller.NewUserController(userSvc)
+
+	// Routes
+	e.POST("/register", userCtrl.Register)
+	e.POST("/login", userCtrl.Login)
 
 	port := os.Getenv("PORT")
 	if port == "" {
