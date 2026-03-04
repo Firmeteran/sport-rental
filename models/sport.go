1267463a6f9e5fc9936ce9769067b11543158870
Firmeteran <mfirmansyah12345@gmail.com>
2026-03-04 09:21:36 +0700

    table struct init

---
+++ models/sport.go
@@ -0,0 +1,36 @@
+package models
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+// User
+type User struct {
+	gorm.Model
+	Username      string  `gorm:"unique;not null" json:"username"`
+	Email         string  `gorm:"unique;not null" json:"email"`
+	Password      string  `gorm:"not null" json:"-"`
+	DepositAmount float64 `gorm:"default:0" json:"deposit_amount"`
+}
+
+// Equipment
+type Equipment struct {
+	gorm.Model
+	Name              string  `gorm:"not null" json:"name"`
+	Category          string  `gorm:"not null" json:"category"`
+	RentalCosts       float64 `gorm:"not null" json:"rental_costs"`
+	StockAvailability int     `gorm:"not null" json:"stock_availability"`
+}
+
+// Rental history
+type RentalHistory struct {
+	gorm.Model
+	UserID      string    `json:"user_id"`
+	EquipmentID string    `json:"equipment_id"`
+	RentDate    time.Time `json:"rent_date"`
+	ReturnDate  time.Time `json:"return_date"`
+	User        User      `gorm:"primaryKey:UserID"`
+	Equipment   Equipment `gorm:"primaryKey:EquipmentID"`
+}
