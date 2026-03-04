50210e314ec5dcc8e24b46be3749e200f4f718a6
Firmeteran <mfirmansyah12345@gmail.com>
2026-03-04 13:03:01 +0700

    controller: user init
    alongside Register and Login function.

---
+++ controller/user_controller.go
@@ -0,0 +1,66 @@
+package controller
+
+import (
+	"net/http"
+	"sport-rental/models"
+	"sport-rental/service"
+
+	"github.com/labstack/echo/v4"
+)
+
+type UserController struct {
+	userService service.UserService
+}
+
+// Constructor
+func NewUserController(s service.UserService) *UserController {
+	return &UserController{userService: s}
+}
+
+// Register - POST /register handler
+func (h *UserController) Register(c echo.Context) error {
+	var user models.User
+
+	// Bind JSON to struct
+	if err := c.Bind(&user); err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]interface{}{"error": "Format data are invalid."})
+	}
+
+	// Call service
+	res, err := h.userService.Register(user)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]interface{}{"error": "User registration failed."})
+	}
+
+	// Return success response
+	return c.JSON(http.StatusCreated, map[string]interface{}{
+		"message": "User registration success.",
+		"data":    res,
+	})
+}
+
+// Login - POST /login handler
+func (h *UserController) Login(c echo.Context) error {
+	var loginData struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	// Bind JSON to struct
+	if err := c.Bind(&loginData); err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]interface{}{"error": "Input invalid."})
+	}
+
+	// Call service
+	user, err := h.userService.Login(loginData.Email, loginData.Password)
+	if err != nil {
+		return c.JSON(http.StatusUnauthorized, map[string]interface{}{"error": err.Error()})
+	}
+
+	// Return success response
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"message": "Login successful",
+		"data":    user,
+	})
+
+}
