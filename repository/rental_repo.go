14fe87832d6a3c6598673fe0a294d0448ad91de3
Firmeteran <mfirmansyah12345@gmail.com>
2026-03-05 12:55:04 +0700

    repo rental: add new functions
    UpdateReturn and GetByID

--- repository/rental_repo.go
+++ repository/rental_repo.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"sport-rental/models"
+	"time"
 
 	"gorm.io/gorm"
 )
@@ -9,6 +10,8 @@ import (
 type RentalRepo interface {
 	CreateRental(rental models.RentalHistory, cost float64) (models.RentalHistory, error)
 	GetRentalByUserID(userID int) ([]models.RentalHistory, error)
+	UpdateReturn(rentalID uint, equipmentID uint) error
+	GetByID(id uint) (models.RentalHistory, error)
 }
 
 type rentalRepo struct {
@@ -50,4 +53,29 @@ func (r *rentalRepo) GetRentalByUserID(userID int) ([]models.RentalHistory, erro
 	// Preload is used to retrieve Equipment data simultaneously
 	err := r.db.Preload("Equipment").Where("user_id = ?", userID).Find(&history).Error
 	return history, err
+}
+
+func (r *rentalRepo) UpdateReturn(rentalID uint, equipmentID uint) error {
+	return r.db.Transaction(func(tx *gorm.DB) error {
+		// Update ReturnDate in rental_histories table
+		now := time.Now()
+		if err := tx.Model(&models.RentalHistory{}).Where("id = ?", rentalID).
+			Update("return_date", &now).Error; err != nil {
+			return err
+		}
+
+		// Adding back stock in equipments table (+ 1)
+		if err := tx.Model(&models.Equipment{}).Where("id = ?", equipmentID).
+			Update("stock_availability", gorm.Expr("stock_availability + ?", 1)).Error; err != nil {
+			return err
+		}
+		return nil
+	})
+}
+
+// Search ID for validation in the service level
+func (r *rentalRepo) GetByID(id uint) (models.RentalHistory, error) {
+	var rental models.RentalHistory
+	err := r.db.First(&rental, id).Error
+	return rental, err
 }
